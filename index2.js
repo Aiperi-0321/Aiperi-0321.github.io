86fe18b2306f21b99410463a4a914fffdd4c3bb2
Aiperi-2103 <aiperiabdulaeva92@.gmail.com>
2024-08-13 19:29:07 -0500

    created addList

---
+++ index2.js
@@ -0,0 +1,19 @@
+function addToList (){
+    const textInput = document.querySelector("input");
+    const text = textInput.value.trim()
+
+if (text){
+    const li = document.createElement("li");
+    li.textContent = text;
+
+const textList = document.querySelector("ul");
+textList.append(li);
+
+const deleteButton = document.createElement("button");
+deleteButton.textContent = "Delete";
+deleteButton.onclick = function(){
+    li.remove();
+};
+li.append(deleteButton);
+}
+}
